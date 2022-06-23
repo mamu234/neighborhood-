3c26e64779991d8e537e6353d78a15cf21618dcf
carolyne <pcmaunda@gmail.com>
2022-06-23 06:59:57 +0300

    test for neigborhood

--- neighborhoododapp/tests.py
+++ neighborhoododapp/tests.py
@@ -28,3 +28,30 @@ class ProfileTestClass(TestCase):
         self.user_profile.delete_profile()
         profiles=Profile.objects.all()
         self.assertTrue(len(profiles)==0)
+class NeighbouhoodTestClass(TestCase):
+    #setup method
+    def setUp(self):
+        self.user = Profile(name='Levy')
+        self.user.save()
+        self.user_profile = Profile(user=self.user,profile_picture="photore.png",bio="My bio")
+        self.hood= NeighbourHood(hood_name="Dala",hood_location="Nairobi",occupants_count=10,admin=self.user_profile)
+        
+   
+    def tearDown(self):
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        NeighbourHood.objects.all().delete()
+      
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hood,NeighbourHood))
+            
+    def test_save_neighbourhood(self):
+        self.hood.save_neighbourhood()
+        hoods=NeighbourHood.objects.all()
+        self.assertTrue(len(hoods)>0)
+        
+    def test_delete_neighbourhood(self):
+        self.hood.save_neighbourhood()
+        self.hood.delete_neighbourhood()
+        hood=NeighbourHood.objects.all()
+        self.assertTrue(len(hood)==0)
